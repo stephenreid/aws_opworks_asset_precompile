f2d8a00d47d7e6cddd628d85e23abead02eb00d8
Stephen Reid <stephen.reid@terminus.com>
2016-08-09 15:11:20 -0400

    MissUse env instead of new_resource.

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,48 +10,51 @@ Chef::Log.info("# AWS Opsworks Asset Precompile #")
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 
 # Precompile assets. Assets are compiled into shared/assets and shared between deploys.
-shared_path = "#{new_resource.deploy_to}/shared"
-resources = [
-	{ release: "public/assets", shared: "assets"},
-	{ release: "node_modules", shared: "node_modules"},
-]
+node[:deploy].each do |application, deploy|
+	shared_path = "#{params[:path]}/shared"
+	release_path = "#{deploy[:deploy_to]}/current/"
+	
+	resources = [
+		{ release: "public/assets", shared: "assets"},
+		{ release: "node_modules", shared: "node_modules"},
+	]
 
-resources.each do |resource|
-	# Erase the repositories public assets if they exist
-	# We'll be creating new compiled assets
-	directory "delete directory" do
-	  recursive true
-	  path "#{release_path}/#{resource[:release]}"
-	  action :delete
+	resources.each do |resource|
+		# Erase the repositories public assets if they exist
+		# We'll be creating new compiled assets
+		directory "delete directory" do
+		  recursive true
+		  path "#{release_path}/#{resource[:release]}"
+		  action :delete
+		end
+
+		# create shared directory for assets, if it doesn't exist
+		# if it already exists, then it will be smarter on only
+		# changed files.
+		directory "#{shared_path}/#{resource[:shared]}" do
+		  mode 0770
+		  action :create
+		  recursive true
+		end
+
+		# Symlink the release path to the shared path
+		link "#{release_path}/#{resource[:release]}" do
+		  to "#{shared_path}/#{resource[:shared]}"
+		end
 	end
 
-	# create shared directory for assets, if it doesn't exist
-	# if it already exists, then it will be smarter on only
-	# changed files.
-	directory "#{shared_path}/#{resource[:shared]}" do
-	  mode 0770
-	  action :create
-	  recursive true
+	execute "npm install" do
+		cwd release_path
+		environment "RAILS_ENV" => rails_env
+		command "npm install --production"
 	end
 
-	# Symlink the release path to the shared path
-	link "#{release_path}/#{resource[:release]}" do
-	  to "#{shared_path}/#{resource[:shared]}"
+	execute "rake assets:precompile" do
+		cwd release_path
+		environment "RAILS_ENV" => ENV["RAILS_ENV"]
+		environment "FOG_DIRECTORY" => ENV["FOG_DIRECTORY"]
+		environment "AWS_ACCESS_KEY" => ENV["AWS_ACCESS_KEY"]
+		environment "AWS_SECRET_ACCESS_KEY" => ENV["AWS_SECRET_ACCESS_KEY"]
+		command "bundle exec rake assets:precompile"
 	end
-end
-
-
-execute "npm install" do
-	cwd release_path
-	environment "RAILS_ENV" => rails_env
-	command "npm install --production"
-end
-
-execute "rake assets:precompile" do
-	cwd release_path
-	environment "RAILS_ENV" => ENV["RAILS_ENV"]
-	environment "FOG_DIRECTORY" => ENV["FOG_DIRECTORY"]
-	environment "AWS_ACCESS_KEY" => ENV["AWS_ACCESS_KEY"]
-	environment "AWS_SECRET_ACCESS_KEY" => ENV["AWS_SECRET_ACCESS_KEY"]
-	command "bundle exec rake assets:precompile"
 end
